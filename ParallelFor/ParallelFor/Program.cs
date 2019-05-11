bdb537ee934130eb4f3fd06d4067df671d42de7b
Mga Khachatryan <magakhachatryan2000@gmail.com>
2019-05-12 02:26:05 +0400

    bla1

---
+++ ParallelFor/ParallelFor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] Arr = { 1, 1, 1, 1, 1, 1, 1, 1, 1,9 };
+            Random rnd = new Random();
+            ParallelOptions opt = new ParallelOptions();
+            opt.MaxDegreeOfParallelism = rnd.Next(2, Arr.Length);
+            int Sum = 0;
+            
+            Parallel.For(0, Arr.Length, opt, index =>
+             {
+
+                 Sum += Arr[index];
+                 ++index;
+             });
+            Console.WriteLine(Sum);
+            
+        }
+    }
+}
